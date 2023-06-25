6f8eeebe18ef254b2aa974e650741ca0fe5efdc8
Kenan <gurbanov.kanan02@gmail.com>
2023-06-25 14:06:10 +0400

    added inital components, httpclient and home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddStudentComponent } from './add-student/add-student.component';
+import { EditStudentComponent } from './edit-student/edit-student.component';
+import { ViewStudentComponent } from './view-student/view-student.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddStudentComponent,
+    EditStudentComponent,
+    ViewStudentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/service/http-provider.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { WebApiService } from './web-api.service';
+import { apiUrl } from '../../environments/environment'
+
+var httpLink = apiUrl+"/students";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpProviderService {
+  constructor(private webApiService: WebApiService) { }
+
+  public getAllStudent(): Observable<any> {
+    return this.webApiService.get(httpLink);
+  }
+  public deleteStudentById(model: any): Observable<any> {
+    return this.webApiService.delete(httpLink + '?studentId=' + model);
+  }
+  public getStudentDetailById(model: any): Observable<any> {
+    return this.webApiService.get(httpLink + '?employeeId=' + model);
+  }
+  public saveStudent(model: any): Observable<any> {
+    return this.webApiService.post(httpLink, model);
+  }  
+}                          
